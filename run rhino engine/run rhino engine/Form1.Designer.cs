415b1ecf9f78247f103e15cd408e02739817d337
nolbo <52912534+nolbo@users.noreply.github.com>
2020-03-12 15:40:54 +0900

    Add files via upload

---
+++ run rhino engine/run rhino engine/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace run_rhino_engine
+{
+    partial class RunningRhinoForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.codeBox = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.runBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // codeBox
+            // 
+            this.codeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.codeBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.codeBox.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeBox.Location = new System.Drawing.Point(0, 57);
+            this.codeBox.Multiline = true;
+            this.codeBox.Name = "codeBox";
+            this.codeBox.Size = new System.Drawing.Size(1532, 781);
+            this.codeBox.TabIndex = 0;
+            this.codeBox.WordWrap = false;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultBox.BackColor = System.Drawing.Color.White;
+            this.resultBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resultBox.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBox.Location = new System.Drawing.Point(0, 844);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(1532, 135);
+            this.resultBox.TabIndex = 1;
+            // 
+            // runBtn
+            // 
+            this.runBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.runBtn.BackColor = System.Drawing.Color.White;
+            this.runBtn.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runBtn.Location = new System.Drawing.Point(0, 1);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(1532, 50);
+            this.runBtn.TabIndex = 2;
+            this.runBtn.Text = "Run!";
+            this.runBtn.UseVisualStyleBackColor = false;
+            this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
+            // 
+            // RunningRhinoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1533, 979);
+            this.Controls.Add(this.runBtn);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.codeBox);
+            this.Name = "RunningRhinoForm";
+            this.Text = "rhino";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox codeBox;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button runBtn;
+    }
+}
+
---
+++ run rhino engine/run rhino engine/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.mozilla.javascript;
+using org.mozilla.javascript.annotations;
+
+namespace run_rhino_engine
+{
+    public partial class RunningRhinoForm : Form
+    {
+        public RunningRhinoForm()
+        {
+            InitializeComponent();
+        }
+
+        Context rhino;
+        Scriptable scope;
+        private void runBtn_Click(object sender, EventArgs e)
+        {
+            rhino = Context.enter();
+            rhino.setOptimizationLevel(-1);
+            rhino.setLanguageVersion(Context.VERSION_ES6);
+            try
+            {
+                scope = new ImporterTopLevel(rhino);
+                ScriptableObject.putProperty(scope, "ctx", this);
+                ScriptableObject.defineClass(scope, new customMethod.Api().getClass());
+                resultBox.Text = rhino.evaluateString(scope, codeBox.Text, "JavaScript", 1, null).ToString();
+
+            }
+            catch(Exception error)
+            {
+                resultBox.Text = error.ToString();
+            }
+            finally
+            {
+                Context.exit();
+            }
+		}
+    }
+
+    public class customMethod
+    {
+        public class Api : ScriptableObject
+        {
+            public override string getClassName()
+            {
+                return "Api";
+            }
+
+            [JSStaticFunction]
+            public static string Test()
+            {
+                return "Hello World!";
+            }
+        }
+    }
+}
